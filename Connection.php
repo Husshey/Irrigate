03a4092920e715d71e7e8b025dcedd2883661c96
Husshey <razsucion99@gmail.com>
2026-05-06 10:00:34 +0800

    update db credentials

--- Connection.php
+++ Connection.php
@@ -1,8 +1,8 @@
 <?php
-$DB_HOST = "localhost";
+$DB_HOST = "mysql.railway.internal";
 $DB_USER = "root";
-$DB_PASS = "";
-$DB_NAME = "irrigation_db";
+$DB_PASS = "dFdVVmMsxnBBDWvLCJTJiFZZVqqNuwVl";
+$DB_NAME = "railway";
 
 $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS);
 
