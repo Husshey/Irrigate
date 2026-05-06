11b99bcd2a8a3edf27958cfb2a29a6de110b9516
Husshey <razsucion99@gmail.com>
2026-05-06 22:32:43 +0800

    update db credentials

--- Connection.php
+++ Connection.php
@@ -4,13 +4,9 @@ $DB_USER = "root";
 $DB_PASS = "dFdVVmMsxnBBDWvLCJTJiFZZVqqNuwVl";
 $DB_NAME = "railway";
 
-$conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS);
+$conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 
 if ($conn->connect_error) {
     $conn = null;
-} else {
-    if (!$conn->select_db($DB_NAME)) {
-        $conn = null;
-    }
 }
 ?>
