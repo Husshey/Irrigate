34370ca60a097ab7e923b9ef0e0595625881d710
Husshey <razsucion99@gmail.com>
2026-05-10 20:13:21 +0800

    Revert changes

--- Connection.php
+++ Connection.php
@@ -1,11 +1,10 @@
 <?php
-$host = getenv('MYSQLHOST')      ?: 'mysql.railway.internal';
+$host = getenv('MYSQLHOST')      ?: 'localhost';
 $port = (int)(getenv('MYSQLPORT') ?: 3306);
 $user = getenv('MYSQLUSER')      ?: 'root';
-$pass = getenv('MYSQLPASSWORD')  ?: 'dFdVVmMsxnBBDWvLCJTJiFZZVqqNuwVl';
-$db   = getenv('MYSQL_DATABASE') ?: 'railway';
+$pass = getenv('MYSQLPASSWORD')  ?: '';
+$db   = getenv('MYSQL_DATABASE') ?: 'irrigation_db';
 
-mysqli_report(MYSQLI_REPORT_OFF); // disable exceptions
 $conn = new mysqli($host, $user, $pass, $db, $port);
 if ($conn->connect_error) $conn = null;
 ?>
