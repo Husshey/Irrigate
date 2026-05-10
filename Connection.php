dddd6725ac54df32b5fb5f841a85b7200c7baff2
Husshey <razsucion99@gmail.com>
2026-05-10 20:25:15 +0800

    Last one

--- Connection.php
+++ Connection.php
@@ -1,11 +1,11 @@
 <?php
+mysqli_report(MYSQLI_REPORT_OFF);
 $host = getenv('MYSQLHOST')      ?: 'localhost';
 $port = (int)(getenv('MYSQLPORT') ?: 3306);
 $user = getenv('MYSQLUSER')      ?: 'root';
 $pass = getenv('MYSQLPASSWORD')  ?: '';
-$db   = getenv('MYSQL_DATABASE') ?: 'irrigation_db';
+$db   = getenv('MYSQL_DATABASE') ?: 'railway';
 
-mysqli_report(MYSQLI_REPORT_OFF); // disable exceptions
 $conn = new mysqli($host, $user, $pass, $db, $port);
 if ($conn->connect_error) $conn = null;
 ?>
