6f24938663c953b838874203fcc2296ec81a2d65
Husshey <razsucion99@gmail.com>
2026-05-10 15:42:25 +0800

    fix mysqli exception mode for PHP 8.3

--- Connection.php
+++ Connection.php
@@ -5,6 +5,7 @@ $user = getenv('MYSQLUSER')      ?: 'root';
 $pass = getenv('MYSQLPASSWORD')  ?: '';
 $db   = getenv('MYSQL_DATABASE') ?: 'irrigation_db';
 
+mysqli_report(MYSQLI_REPORT_OFF); // disable exceptions
 $conn = new mysqli($host, $user, $pass, $db, $port);
 if ($conn->connect_error) $conn = null;
 ?>
