3ead51f2e10df6f8653a29b07afd94f8f3056167
Husshey <razsucion99@gmail.com>
2026-05-11 08:11:04 +0800

    fix db env var name

--- Connection.php
+++ Connection.php
@@ -4,7 +4,7 @@ $host = getenv('MYSQLHOST')      ?: 'localhost';
 $port = (int)(getenv('MYSQLPORT') ?: 3306);
 $user = getenv('MYSQLUSER')      ?: 'root';
 $pass = getenv('MYSQLPASSWORD')  ?: '';
-$db   = getenv('MYSQL_DATABASE') ?: 'railway';
+$db = getenv('MYSQLDATABASE') ?: 'railway';
 
 $conn = new mysqli($host, $user, $pass, $db, $port);
 if ($conn->connect_error) $conn = null;
